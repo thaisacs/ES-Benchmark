70fee839dfb7a1b5ba1fa3a0538838c9932388b7
thais <thaiscamachoo@gmail.com>
2020-05-29 11:50:27 -0300

    experiments updated and gitignore added

--- NPB3.4-MPI/experiments/script-build.sh
+++ NPB3.4-MPI/experiments/script-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd ..
-mkdir -p bin
-cd kernel_stats
+mkdir -p ../bin
+cd ../kernel_stats
 make
 cd ..
+for bench in cg ep is mg ft; do
+  for size in C D E; do
+	echo "$bench	$size" >> config/suite.def
+  done
+done
 make suite
--- NPB3.4-MPI/experiments/script-experiment.sh
+++ NPB3.4-MPI/experiments/script-experiment.sh
@@ -3,7 +3,8 @@
 mkdir -p results
 cd results
 
-/usr/bin/time -o CG-A-TIME.out -p  mpirun -n 2 ../../bin/cg.A.x 1> CG-A.out 2> CG-A.err
-/usr/bin/time -o CG-B-TIME.out -p  mpirun -n 2 ../../bin/cg.B.x 1> CG-B.out 2> CG-B.err
-/usr/bin/time -o CG-C-TIME.out -p  mpirun -n 2 ../../bin/cg.C.x 1> CG-C.out 2> CG-C.err
-#/usr/bin/time -o CG-D-TIME.out -p  mpirun -n 2 ../bin/cg.D.x 1> CG-D.out 2> CG-D.err
+for bench in cg ep is mg ft; do
+  for size in C D E; do
+	/usr/bin/time -o $bench-$size-TIME.out -p  mpirun -n 2 ../../bin/$bench.$size.x 1> $bench-$size.out 2> $bench-$size.err
+  done
+done
